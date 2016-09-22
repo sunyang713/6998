9692a9e03fd0d0e015b25e3ed13b330d7a8db28a
Iris <iz2140@columbia.edu>
2016-09-22 14:51:34 -0400

    Adding validation checker

--- src/coms6998/fall2016/lambdaFunctions/CreateCustomerLambdaFunctionHandler.java
+++ src/coms6998/fall2016/lambdaFunctions/CreateCustomerLambdaFunctionHandler.java
@@ -2,9 +2,9 @@ package coms6998.fall2016.lambdaFunctions;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-//import com.google.gson.Gson;
 
 import coms6998.fall2016.managers.DynamoDBManager;
+import coms6998.fall2016.managers.ValidationManager;
 import coms6998.fall2016.models.Customer;
 import coms6998.fall2016.models.Response;
 
@@ -13,16 +13,15 @@ public class CreateCustomerLambdaFunctionHandler implements RequestHandler<Custo
     @Override
     public String handleRequest(Customer input, Context context) {
         context.getLogger().log("Input: " + input);
+        ValidationManager vm = new ValidationManager(context);
         
-        //Gson myGson = new Gson();
-        
-        //Response obj = null;
-        
-        boolean success = (new DynamoDBManager()).createCustomer(input);
+        boolean success = vm.isValidCustomer(input) && (new DynamoDBManager()).create(input);
 
         if(success) {
+        	//return customer
         	return "{\"message\":\"Customer with email: " + input.getEmail() + " created.\"}";
         } else {
+        	//throw runtime exception error w error model object
         	return "{\"message\":\"Failed to create Customer with email: " + input.getEmail() + ".\"}";
         }
     }
