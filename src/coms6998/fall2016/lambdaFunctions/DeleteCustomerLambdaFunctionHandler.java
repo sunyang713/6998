bc0b97fe5a8bfd8b28c855b10001fc93dc8822ec
Gaurav Singhal <gaurav.myr@gmail.com>
2016-09-28 10:19:21 -0400

    add DBManager interface and make DynamoDBManager interface implement that interface and use DBManager references in lambda functions

--- src/coms6998/fall2016/lambdaFunctions/DeleteCustomerLambdaFunctionHandler.java
+++ src/coms6998/fall2016/lambdaFunctions/DeleteCustomerLambdaFunctionHandler.java
@@ -4,15 +4,18 @@ package coms6998.fall2016.lambdaFunctions;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+import coms6998.fall2016.managers.DBManager;
 import coms6998.fall2016.managers.DynamoDBManager;
 import coms6998.fall2016.models.Customer;
 
 public class DeleteCustomerLambdaFunctionHandler implements RequestHandler<Customer, String> {
 	
+	private DBManager dbManager = new DynamoDBManager();
+	
     @Override
     public String handleRequest(Customer customer, Context context) {
         context.getLogger().log("Deleting customer with email: " + customer.getEmail());
-        boolean rval = (new DynamoDBManager()).deleteCustomer(customer);
+        boolean rval = dbManager.deleteCustomer(customer);
         if(rval) {
         	return "{\"message\":\"Customer with email: " + customer.getEmail() + " deleted.\"}";
         } else {
